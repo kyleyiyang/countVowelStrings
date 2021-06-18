94b8bd25655f8840d3b588583c94a3c9f65fbf92
kyleyiyang <kyleyiyang@gmail.com>
2021-06-17 21:51:49 -0400

    Update countVowelStrings.java

--- countVowelStrings.java
+++ countVowelStrings.java
@@ -11,3 +11,17 @@ class Solution {
         return permutation[0];
     }
 }
+
+******************
+
+class Solution {
+    public int countVowelStrings(int n) {
+        int[] permutation = new int[]{1,1,1,1,1};
+        for (int i=0;i<n;i++) {
+            for (int j=permutation.length-2;j>=0;j--) {
+                permutation[j]+=permutation[j+1];
+            }
+        }
+        return permutation[0];
+    }
+}
